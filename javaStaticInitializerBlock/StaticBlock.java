a4857e8d69d3dafa5279f17c66dfe4c1a5581ed6
mridulraturi7 <mridulraturi7@gmail.com>
2020-05-22 12:54:24 +0530

    Implemented Java Static Initializer Block

---
+++ javaStaticInitializerBlock/StaticBlock.java
@@ -0,0 +1,35 @@
+package javaStaticInitializerBlock;
+
+import java.util.Scanner;
+
+public class StaticBlock {
+    
+    static boolean flag=true;
+    static int B;
+    static int H;
+
+    static
+    {
+        Scanner scanner = new Scanner(System.in);
+        B = scanner.nextInt();
+        H = scanner.nextInt();
+
+        if(B<=0 || H<=0)
+        {
+            flag=false;
+            System.out.println("java.lang.Exception: Breadth and height must be positive");
+        }
+
+    scanner.close();
+
+    }
+
+    public static void main(String[] args){
+		if(flag){
+			int area=B*H;
+			System.out.print(area);
+		}
+		
+	}//end of main
+
+}
