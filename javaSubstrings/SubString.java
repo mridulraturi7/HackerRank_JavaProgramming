f632a8e88a33000ede307cc9dff28ee21ea972e4
mridulraturi7 <mridulraturi7@gmail.com>
2020-05-23 11:23:24 +0530

    Implemented Java Substring

---
+++ javaSubstrings/SubString.java
@@ -0,0 +1,18 @@
+package javaSubstrings;
+
+import java.util.Scanner;
+
+public class SubString {
+    
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+        int start = in.nextInt();
+        int end = in.nextInt();
+
+        in.close();
+        
+        System.out.println(S.substring(start,end));
+    }
+
+}
