5aa5664f9d029b6c79340488485f1717f0f5d3a8
mridulraturi7 <mridulraturi7@gmail.com>
2020-05-24 18:54:15 +0530

    Implemented Java String Reverse

---
+++ javaStringReverse/StringReverse.java
@@ -0,0 +1,39 @@
+package javaStringReverse;
+
+import java.util.Scanner;
+
+public class StringReverse {
+    
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        /* Enter your code here. Print output to STDOUT. */
+
+        String C = "";
+
+        for(int i=A.length()-1;i>=0;i--)
+        {
+            for(int j=0;j<A.length();j++)
+            {
+                char[] B = A.toCharArray();
+                B[j] = A.charAt(i);
+                C = String.valueOf(B);
+            }
+        }
+
+        if(A.equals(C))
+        {
+            System.out.println("Yes");
+        }
+
+        else
+        {
+            System.out.println("No");
+        }
+
+        sc.close();
+        
+    }
+
+}
