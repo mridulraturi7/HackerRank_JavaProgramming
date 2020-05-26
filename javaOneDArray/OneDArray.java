ec1f7e3fa3ec52481acecf15bbee3ab30a4901f4
mridulraturi7 <mridulraturi7@gmail.com>
2020-05-26 23:45:15 +0530

    Implemented Java 1-D Array

---
+++ javaOneDArray/OneDArray.java
@@ -0,0 +1,29 @@
+package javaOneDArray;
+
+import java.util.Scanner;
+
+public class OneDArray {
+    
+    public static void main(String[] args) {
+	   
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+    int[] a = new int[n];
+
+    int j = 0;
+    while(scan.hasNext())
+    {
+        a[j] = scan.nextInt();
+        j++;
+    }
+
+    scan.close();
+
+        // Prints each sequential element in array a
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+    }
+
+}
