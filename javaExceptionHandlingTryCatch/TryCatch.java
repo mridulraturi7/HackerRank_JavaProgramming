7186c47a453e441fabd49e48aa420448346a03c4
mridulraturi7 <mridulraturi7@gmail.com>
2020-05-29 18:40:06 +0530

    Implemented Java Exception Handling(Try - Catch)

---
+++ javaExceptionHandlingTryCatch/TryCatch.java
@@ -0,0 +1,33 @@
+package javaExceptionHandlingTryCatch;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class TryCatch {
+    
+    public static void main(String[] args) {
+        
+        try{
+            Scanner scan = new Scanner(System.in);
+
+            int x = scan.nextInt();
+            int y = scan.nextInt();
+
+            if(y == 0)
+            {
+                throw new ArithmeticException("/ by zero");
+            }
+
+            else
+            {
+                System.out.println(x/y);
+            }
+        }catch(InputMismatchException me){
+                System.out.println(me.getClass().getName());
+        }catch(ArithmeticException ae){
+            System.out.println(ae);
+        }
+
+    }
+
+}
