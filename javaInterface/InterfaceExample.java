1f27e092bf3ea65f5448957e886b5583a86b9636
mridulraturi7 <mridulraturi7@gmail.com>
2020-05-30 23:37:15 +0530

    Minor Change in InterfaceExample.java

--- javaInterface/InterfaceExample.java
+++ javaInterface/InterfaceExample.java
@@ -3,14 +3,17 @@ package javaInterface;
 import java.util.Scanner;
 
 interface AdvancedArithmetic{
+
     int divisor_sum(int n);
-  }
+  
+}
   
 class MyCalculator implements AdvancedArithmetic
 {
     public int divisor_sum(int n)
     {
         int sum = 0;
+        
         for(int i = 1; i <= n; i++)
         {
           if(n%i == 0)
@@ -18,7 +21,9 @@ class MyCalculator implements AdvancedArithmetic
             sum = sum + i;
           }
         }
+        
         return sum;
+    
     }
   }
 
@@ -26,20 +31,27 @@ public class InterfaceExample {
 
     public static void main(String []args){
         MyCalculator my_calculator = new MyCalculator();
+        
         System.out.print("I implemented: ");
+        
         ImplementedInterfaceNames(my_calculator);
+        
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        
         System.out.print(my_calculator.divisor_sum(n) + "\n");
       	sc.close();
     }
 
     static void ImplementedInterfaceNames(Object o){
+        
         Class[] theInterfaces = o.getClass().getInterfaces();
+        
         for (int i = 0; i < theInterfaces.length; i++){
             String interfaceName = theInterfaces[i].getName();
             System.out.println(interfaceName);
         }
-    }
     
+    }
+      
 }
