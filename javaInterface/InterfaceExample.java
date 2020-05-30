cfbe5cbcb9e5f489c00b4c0babd45517c7de4044
mridulraturi7 <mridulraturi7@gmail.com>
2020-05-30 23:35:15 +0530

    Modified Indentation in InterfaceExample.java

--- javaInterface/InterfaceExample.java
+++ javaInterface/InterfaceExample.java
@@ -6,20 +6,20 @@ interface AdvancedArithmetic{
     int divisor_sum(int n);
   }
   
-  class MyCalculator implements AdvancedArithmetic
-  {
-      public int divisor_sum(int n)
-      {
-          int sum = 0;
-          for(int i = 1; i <= n; i++)
+class MyCalculator implements AdvancedArithmetic
+{
+    public int divisor_sum(int n)
+    {
+        int sum = 0;
+        for(int i = 1; i <= n; i++)
+        {
+          if(n%i == 0)
           {
-              if(n%i == 0)
-              {
-                  sum = sum + i;
-              }
+            sum = sum + i;
           }
-          return sum;
-      }
+        }
+        return sum;
+    }
   }
 
 public class InterfaceExample {
