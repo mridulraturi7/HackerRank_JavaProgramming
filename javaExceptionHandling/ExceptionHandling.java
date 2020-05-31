9e22c481e465c5bd19846c5467d1486a9a998cc6
mridulraturi7 <mridulraturi7@gmail.com>
2020-05-31 23:36:04 +0530

    MyCalculator class in ExceptionHandling.java

--- javaExceptionHandling/ExceptionHandling.java
+++ javaExceptionHandling/ExceptionHandling.java
@@ -1,5 +1,33 @@
 package javaExceptionHandling;
 
+import java.util.Scanner;
+import java.lang.Math;
+
+class MyCalculator {
+    /*
+    * Create the method long power(int, int) here.
+    */
+    
+    long power(int n, int p) throws Exception
+    {
+        if(n < 0 || p < 0){
+            throw new Exception("n or p should not be negative.");
+        }
+
+        else if(n == 0 && p == 0)
+        {
+            throw new Exception("n and p should not be zero.");
+        }
+
+        else
+        {
+            return (long)Math.pow(n,p);
+        }
+
+    }
+    
+}
+
 public class ExceptionHandling
 {
 
