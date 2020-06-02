ccc9e0966ddd09aee6ea3d57aba129d9cd8e30fb
mridulraturi7 <mridulraturi7@gmail.com>
2020-06-02 18:28:37 +0530

    [Create Soccer class] MethodOverriding.java

--- javaMethodOverriding/MethodOverriding.java
+++ javaMethodOverriding/MethodOverriding.java
@@ -11,6 +11,23 @@ class Sports{
     }
 }
 
+class Soccer extends Sports{
+    
+    @Override
+    String getName(){
+        return "Soccer Class";
+    }
+
+    @Override
+    void getNumberOfTeamMembers()
+    {
+        System.out.println("Each team has 11 players in " + getName());
+    }
+
+    // Write your overridden getNumberOfTeamMembers method here
+
+}
+
 public class MethodOverriding
 {
     public static void main(String []args){
