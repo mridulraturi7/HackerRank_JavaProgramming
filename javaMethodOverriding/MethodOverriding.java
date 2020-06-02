66f015f6815d6efcbaa265be6cc834362ca41f4f
mridulraturi7 <mridulraturi7@gmail.com>
2020-06-02 18:30:06 +0530

    [Minor Fix] MethodOverriding.java

--- javaMethodOverriding/MethodOverriding.java
+++ javaMethodOverriding/MethodOverriding.java
@@ -2,11 +2,13 @@ package javaMethodOverriding;
 
 class Sports{
 
-    String getName(){
+    String getName()
+    {
         return "Generic Sports";
     }
   
-    void getNumberOfTeamMembers(){
+    void getNumberOfTeamMembers()
+    {
         System.out.println( "Each team has n players in " + getName() );
     }
 }
@@ -14,7 +16,8 @@ class Sports{
 class Soccer extends Sports{
     
     @Override
-    String getName(){
+    String getName()
+    {
         return "Soccer Class";
     }
 
@@ -24,19 +27,20 @@ class Soccer extends Sports{
         System.out.println("Each team has 11 players in " + getName());
     }
 
-    // Write your overridden getNumberOfTeamMembers method here
-
 }
 
 public class MethodOverriding
 {
-    public static void main(String []args){
-    Sports c1 = new Sports();
-    Soccer c2 = new Soccer();
-    System.out.println(c1.getName());
-    c1.getNumberOfTeamMembers();
-    System.out.println(c2.getName());
-    c2.getNumberOfTeamMembers();
+    public static void main(String []args)
+    {
+        Sports c1 = new Sports();
+        Soccer c2 = new Soccer();
+
+        System.out.println(c1.getName());
+        c1.getNumberOfTeamMembers();
+        
+        System.out.println(c2.getName());
+        c2.getNumberOfTeamMembers();
 	}
 
 }
