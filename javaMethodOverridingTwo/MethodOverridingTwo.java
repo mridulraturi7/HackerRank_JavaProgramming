14c2af7ead6c03537384f6de8d7ea5dc20ba9e49
mridulraturi7 <mridulraturi7@gmail.com>
2020-06-03 18:38:24 +0530

    Implemented Java Method Overriding Two

--- javaMethodOverridingTwo/MethodOverridingTwo.java
+++ javaMethodOverridingTwo/MethodOverridingTwo.java
@@ -8,6 +8,21 @@ class BiCycle{
     
 }
 
+class MotorCycle extends BiCycle{
+	String define_me(){
+		return "a cycle with an engine.";
+	}
+	
+	MotorCycle(){
+		System.out.println("Hello I am a motorcycle, I am "+ define_me());
+
+		String temp=super.define_me(); //Fix this line
+
+		System.out.println("My ancestor is a cycle who is "+ temp );
+	}
+	
+}
+
 public class MethodOverridingTwo {
     
     public static void main(String []args){
