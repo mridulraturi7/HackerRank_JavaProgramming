6278e518f34cc8894d7f6fd639f1b10cad7cca95
mridulraturi7 <mridulraturi7@gmail.com>
2020-06-04 23:37:29 +0530

    [Minor Change] SubArray.java

--- javaSubArray/SubArray.java
+++ javaSubArray/SubArray.java
@@ -5,8 +5,7 @@ import java.util.Scanner;
 public class SubArray {
 
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
-
+        
         Scanner scan = new Scanner(System.in);
         int n = scan.nextInt();
 
