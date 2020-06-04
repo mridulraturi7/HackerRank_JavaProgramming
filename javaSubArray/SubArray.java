0bf77f98fb78d9c648c5588d48cf65d2ac979ad2
mridulraturi7 <mridulraturi7@gmail.com>
2020-06-04 23:32:47 +0530

    [Created main method] SubArray.java

--- javaSubArray/SubArray.java
+++ javaSubArray/SubArray.java
@@ -2,6 +2,22 @@ package javaSubArray;
 
 public class SubArray {
 
-    
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+        int l = 0;
+        int[] array = new int[n];
+        while(n>0)
+        {
+            array[l] = scan.nextInt();
+            l++;
+            n--;
+        }
+
+        
+    }
     
 }
