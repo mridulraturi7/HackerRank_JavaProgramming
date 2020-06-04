ec766f0e2f3c5e118c289d2693894a0fe2921a24
mridulraturi7 <mridulraturi7@gmail.com>
2020-06-04 12:21:41 +0530

    [Create Main method] BigDecimlaExample.java

--- javaBigDecimal/BigDecimalExample.java
+++ javaBigDecimal/BigDecimalExample.java
@@ -2,6 +2,15 @@ package javaBigDecimal;
 
 public class BigDecimalExample {
 
-    
+    public static void main(String []args){
+        //Input
+        Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt();
+        String []s=new String[n+2];
+        for(int i=0;i<n;i++){
+            s[i]=sc.next();
+        }
+        sc.close();
+    }
     
 }
