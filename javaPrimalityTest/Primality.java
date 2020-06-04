1f7ed07b80789f34cc6ea0eb4b136424d54e253b
mridulraturi7 <mridulraturi7@gmail.com>
2020-06-04 12:30:32 +0530

    [Create Main method] Primality.java

--- javaPrimalityTest/Primality.java
+++ javaPrimalityTest/Primality.java
@@ -2,6 +2,12 @@ package javaPrimalityTest;
 
 public class Primality {
 
-    
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String n = scanner.nextLine();
+
+        scanner.close();
+    }
     
 }
