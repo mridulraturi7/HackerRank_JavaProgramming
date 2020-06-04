c42f18d8dd51cd08a5a1f1954c13fb1dbf8416b1
mridulraturi7 <mridulraturi7@gmail.com>
2020-06-04 12:25:50 +0530

    Implemented Java BigDecimal

--- javaBigDecimal/BigDecimalExample.java
+++ javaBigDecimal/BigDecimalExample.java
@@ -1,5 +1,7 @@
 package javaBigDecimal;
 
+import java.math.BigDecimal;
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class BigDecimalExample {
@@ -13,6 +15,24 @@ public class BigDecimalExample {
             s[i]=sc.next();
         }
         sc.close();
+
+        Comparator<String> myComparator = new Comparator<String>()
+        {
+            @Override
+            public int compare(String s1, String s2)
+            {
+                BigDecimal a = new BigDecimal(s1);
+                BigDecimal b = new BigDecimal(s2);
+                return b.compareTo(a);
+            }
+        };
+
+        Arrays.sort(s, 0 , n, myComparator);
+
+        for(int i = 0; i < n; i++)
+        {
+            System.out.println(s[i]);
+        }
     }
     
 }
