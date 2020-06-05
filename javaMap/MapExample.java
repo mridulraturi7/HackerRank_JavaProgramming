531a56d7732b954cf80d39834f39405ab8072775
mridulraturi7 <mridulraturi7@gmail.com>
2020-06-05 23:58:44 +0530

    [Create main method] MapExample.java

--- javaMap/MapExample.java
+++ javaMap/MapExample.java
@@ -2,6 +2,21 @@ package javaMap;
 
 public class MapExample {
 
-    
+    public static void main(String []argh)
+	{
+		Scanner in = new Scanner(System.in);
+		int n=in.nextInt();
+		in.nextLine();
+		for(int i=0;i<n;i++)
+		{
+			String name=in.nextLine();
+			int phone=in.nextInt();
+			in.nextLine();
+		}
+		while(in.hasNext())
+		{
+			String s=in.nextLine();
+		}
+	}
     
 }
