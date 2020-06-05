a7aa2e1a04396b86345f39e6bc78b3cacdd35cfb
mridulraturi7 <mridulraturi7@gmail.com>
2020-06-05 23:36:37 +0530

    [Create] ArrayListExample.java

---
+++ javaArrayList/ArrayListExample.java
@@ -0,0 +1,48 @@
+package javaArrayList;
+
+public class ArrayListExample {
+
+    private static ArrayList<ArrayList<Integer>> myList = new ArrayList<ArrayList<Integer>>();
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        //scan.nextLine();
+        int j = 0;
+
+        while(n>0)
+        {
+            int m = scan.nextInt();
+
+            if(m == 0)
+            {
+                myList.add(new ArrayList<Integer>());
+            }
+            
+            else
+            {
+                for(int i = 0; i < m; i++)
+            {
+                int val = scan.nextInt();
+                myList.add(new ArrayList<Integer>());
+                myList.get(j).add(val);
+            }
+            }
+            
+            j++;
+            n--;
+        }
+
+        int q = scan.nextInt();
+        while(q>0)
+        {
+            int x = scan.nextInt();
+            int y = scan.nextInt();
+            check(x,y);
+            q--;
+        }
+    }
+    
+}
