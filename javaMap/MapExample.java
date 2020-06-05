5e34bfcac25e6ad5738a277dbb477bcb36aae986
mridulraturi7 <mridulraturi7@gmail.com>
2020-06-05 23:59:10 +0530

    Implemented Java Map

--- javaMap/MapExample.java
+++ javaMap/MapExample.java
@@ -4,6 +4,7 @@ public class MapExample {
 
     public static void main(String []argh)
 	{
+        Map<String, Integer> myMap = new HashMap<>();
 		Scanner in = new Scanner(System.in);
 		int n=in.nextInt();
 		in.nextLine();
@@ -11,11 +12,20 @@ public class MapExample {
 		{
 			String name=in.nextLine();
 			int phone=in.nextInt();
+            myMap.put(name, phone);
 			in.nextLine();
 		}
 		while(in.hasNext())
 		{
 			String s=in.nextLine();
+            if(myMap.containsKey(s))
+            {
+                System.out.println(s + "=" + myMap.get(s));
+            }
+            else
+            {
+                System.out.println("Not found");
+            }
 		}
 	}
     
