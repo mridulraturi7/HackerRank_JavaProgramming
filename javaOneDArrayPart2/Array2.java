5d6651564511b02be5c2b572cd01b7aa7d45e9b1
mridulraturi7 <mridulraturi7@gmail.com>
2020-06-06 23:47:46 +0530

    [Create main method] Array2.java

--- javaOneDArrayPart2/Array2.java
+++ javaOneDArrayPart2/Array2.java
@@ -2,6 +2,20 @@ package javaOneDArrayPart2;
 
 public class Array2 {
 
-    
+    Scanner scan = new Scanner(System.in);
+        int q = scan.nextInt();
+        while (q-- > 0) {
+            int n = scan.nextInt();
+            int leap = scan.nextInt();
+            
+            int[] game = new int[n];
+            for (int i = 0; i < n; i++) {
+                game[i] = scan.nextInt();
+            }
+
+            System.out.println( (canWin(leap, game)) ? "YES" : "NO" );
+        }
+        scan.close();
+    }
     
 }
