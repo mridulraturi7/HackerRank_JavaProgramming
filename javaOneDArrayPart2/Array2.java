c9b0f7763ad0e7586efa09da98610b576c335192
mridulraturi7 <mridulraturi7@gmail.com>
2020-06-06 23:50:22 +0530

    [canWin() method] Array2.java

--- javaOneDArrayPart2/Array2.java
+++ javaOneDArrayPart2/Array2.java
@@ -4,6 +4,18 @@ import java.util.Scanner;
 
 public class Array2 {
 
+    public static boolean canWin(int leap, int[] game) {
+        // Return true if you can win the game; otherwise, return false.
+
+        if(game == null)
+        {
+            return false;
+        }
+
+        return check(leap, game, 0);
+
+    }
+
     public static void main(String[] args) {
         
         Scanner scan = new Scanner(System.in);
