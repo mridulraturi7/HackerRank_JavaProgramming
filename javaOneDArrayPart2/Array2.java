6f9ac476baca947e447ea21efc434bf76ce09950
mridulraturi7 <mridulraturi7@gmail.com>
2020-06-06 23:51:20 +0530

    Implemented Java 1D Array Part 2

--- javaOneDArrayPart2/Array2.java
+++ javaOneDArrayPart2/Array2.java
@@ -16,6 +16,23 @@ public class Array2 {
 
     }
 
+    public static boolean check(int leap, int[] game, int i)
+    {
+        if(i >= game.length)
+        {
+            return true;
+        }
+
+        else if(i < 0 || game[i] == 1)
+        {
+            return false;
+        }
+
+        game[i] = 1;
+
+        return check(leap, game, i + leap) || check(leap, game, i + 1) || check(leap, game, i - 1);
+    }
+
     public static void main(String[] args) {
         
         Scanner scan = new Scanner(System.in);
