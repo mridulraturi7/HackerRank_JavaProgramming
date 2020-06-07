320c9b0cd5a91b382a78451f982f1aa1d1252e17
mridulraturi7 <mridulraturi7@gmail.com>
2020-06-07 21:17:56 +0530

    [Create main method] BalancedStack.java

--- javaStack/BalancedStack.java
+++ javaStack/BalancedStack.java
@@ -2,6 +2,26 @@ package javaStack;
 
 public class BalancedStack {
 
-    
+    public static void main(String []argh)
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		while (sc.hasNext()) {
+			String input=sc.next();
+            //Complete the code
+            boolean ch = check(input);
+
+            if(ch == true)
+            {
+                System.out.println("true");
+            }
+
+            else
+            {
+                System.out.println("false");
+            }
+		}
+		
+	}
     
 }
