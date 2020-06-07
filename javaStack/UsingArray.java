d73e6610639ffe66a669bda7b8d5149b9915e4e4
mridulraturi7 <mridulraturi7@gmail.com>
2020-06-07 21:31:27 +0530

    Modified indentation UsingArray.java

--- javaStack/UsingArray.java
+++ javaStack/UsingArray.java
@@ -9,8 +9,8 @@ public class UsingArray {
 		Scanner sc = new Scanner(System.in);
 		
 		while (sc.hasNext()) {
+
 			String input=sc.next();
-            //Complete the code
             boolean ch = check(input);
 
             if(ch == true)
@@ -28,70 +28,67 @@ public class UsingArray {
     
     public static boolean check(String str)
     {
+
         int length = str.length();
-        //int mid = length/2;
-        //int push = 0, pop = 0;
         char[] array = str.toCharArray();
 
-        //Stack<Character> myStack = new Stack<Character>();
-
-    for(int i = 0; i < length; i++)
-    {
-        if(array[i] == '(')
+        for(int i = 0; i < length; i++)
         {
-            //myStack.push(array[i]);
-            for(int j = i+1; j < length; j++)
+            if(array[i] == '(')
             {
-                if(array[j] == ')')
+                //myStack.push(array[i]);
+                for(int j = i+1; j < length; j++)
                 {
-                    array[i] = 'v';
-                    array[j] = 'v';
-                    break;
-                    //char ch = myStack.pop();
+                    if(array[j] == ')')
+                    {
+                        array[i] = 'v';
+                        array[j] = 'v';
+                        break;
+                        //char ch = myStack.pop();
+                    }
+                }
+            }
+
+            else if(array[i] == '{')
+            {
+                //myStack.push(array[i]);
+                for(int j = i+1; j < length; j++)
+                {
+                    if(array[j] == '}')
+                    {
+                        array[j] = 'v';
+                        array[i] = 'v';
+                        break;
+                        //char ch = myStack.pop();
+                    }
+                }
+            }
+
+            else if(array[i] == '[')
+            {
+                //myStack.push(array[i]);
+                for(int j = i+1; j < length; j++)
+                {
+                    if(array[j] == ']')
+                    {
+                        array[j] = 'v';
+                        array[i] = 'v';
+                        break;
+                        //char ch = myStack.pop();
+                    }
                 }
             }
         }
 
-        else if(array[i] == '{')
+        for(int i = 0; i < length; i++)
         {
-            //myStack.push(array[i]);
-            for(int j = i+1; j < length; j++)
+            if(array[i] != 'v')
             {
-                if(array[j] == '}')
-                {
-                    array[j] = 'v';
-                    array[i] = 'v';
-                    break;
-                    //char ch = myStack.pop();
-                }
+                return false;
             }
         }
 
-        else if(array[i] == '[')
-        {
-            //myStack.push(array[i]);
-            for(int j = i+1; j < length; j++)
-            {
-                if(array[j] == ']')
-                {
-                    array[j] = 'v';
-                    array[i] = 'v';
-                    break;
-                    //char ch = myStack.pop();
-                }
-            }
-        }
-    }
-
-    for(int i = 0; i < length; i++)
-    {
-        if(array[i] != 'v')
-        {
-            return false;
-        }
-    }
-
-    return true;
+        return true;
 
     }
     
