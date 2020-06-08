28904af339fe6f33f683d37daed4d35e0fd3ec18
mridulraturi7 <mridulraturi7@gmail.com>
2020-06-08 23:15:38 +0530

    [Create main method] HashSetExample.java

--- javaHashSet/HashSetExample.java
+++ javaHashSet/HashSetExample.java
@@ -2,6 +2,16 @@ package javaHashSet;
 
 public class HashSetExample {
 
-    
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int t = s.nextInt();
+        String [] pair_left = new String[t];
+        String [] pair_right = new String[t];
+        
+        for (int i = 0; i < t; i++) {
+            pair_left[i] = s.next();
+            pair_right[i] = s.next();
+        }
+   }
     
 }
