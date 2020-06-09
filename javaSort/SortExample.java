70a070e1e3a3bcde9ec9c67c5adababed610f346
mridulraturi7 <mridulraturi7@gmail.com>
2020-06-09 12:30:47 +0530

    [Resolved Imports] SortExample.java

--- javaSort/SortExample.java
+++ javaSort/SortExample.java
@@ -1,10 +1,6 @@
 package javaSort;
 
-import java.util.Scanner;
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.Collections;
+import java.util.*;
 
 class Student
 {
