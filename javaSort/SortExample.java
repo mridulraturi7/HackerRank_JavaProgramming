5bd8aa5019572deec136aa2778a861dcb4944bf5
mridulraturi7 <mridulraturi7@gmail.com>
2020-06-09 12:14:32 +0530

    [Create main method] SortExample.java

--- javaSort/SortExample.java
+++ javaSort/SortExample.java
@@ -2,6 +2,25 @@ package javaSort;
 
 public class SortExample {
 
-    
+    public static void main(String[] args){
+		Scanner in = new Scanner(System.in);
+		int testCases = Integer.parseInt(in.nextLine());
+		
+		List<Student> studentList = new ArrayList<Student>();
+		while(testCases>0){
+			int id = in.nextInt();
+			String fname = in.next();
+			double cgpa = in.nextDouble();
+			
+			Student st = new Student(id, fname, cgpa);
+			studentList.add(st);
+			
+			testCases--;
+		}
+      
+      	for(Student st: studentList){
+			System.out.println(st.getFname());
+		}
+	}
     
 }
