c2f9187b1d656ffcdedb68aa8a51cde3d55e8243
mridulraturi7 <mridulraturi7@gmail.com>
2020-06-10 18:34:43 +0530

    [Logic Done] DequeExample.java

--- javaDeque/DequeExample.java
+++ javaDeque/DequeExample.java
@@ -8,9 +8,29 @@ public class DequeExample {
         int n = in.nextInt();
         int m = in.nextInt();
 
+        HashSet<Integer> myHash = new HashSet<>();
+        int unique = 0;
+
         for (int i = 0; i < n; i++) {
             int num = in.nextInt();
+
+            deque.add(num);
+            myHash.add(num);
+
+            if(deque.size() == m)
+            {
+                if(myHash.size() > unique)
+                {
+                    unique = myHash.size();
+                }
+                int f = deque.remove();
+                if(!deque.contains(f))
+                {
+                    myHash.remove(f);
+                }
+            }
         }
+        System.out.println(unique);
     }            
     
 }
