1057d7fd2cdd3dc4b9df18ce8260335a70562d4b
mridulraturi7 <mridulraturi7@gmail.com>
2020-06-11 18:25:47 +0530

    [Create main method] BitSetExample.java

--- javaBitSet/BitSetExample.java
+++ javaBitSet/BitSetExample.java
@@ -2,6 +2,11 @@ package javaBitSet;
 
 public class BitSetExample {
 
-    
-    
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int m = scan.nextInt();
+    }
 }
