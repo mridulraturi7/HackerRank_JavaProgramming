e2944de49dbc262ac98289fa158502246693601a
mridulraturi7 <mridulraturi7@gmail.com>
2020-06-12 18:04:58 +0530

    [Modify] PriorityQueueExample.java

--- javaPriorityQueue/PriorityQueueExample.java
+++ javaPriorityQueue/PriorityQueueExample.java
@@ -1,7 +1,33 @@
 package javaPriorityQueue;
 
-public class PriorityQueueExample {
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+/*
+ * Create the Student and Priorities classes here.
+ */
 
+public class PriorityQueueExample {
+    private final static Scanner scan = new Scanner(System.in);
+    private final static Priorities priorities = new Priorities();
     
-    
+    public static void main(String[] args) {
+        int totalEvents = Integer.parseInt(scan.nextLine());    
+        List<String> events = new ArrayList<>();
+        
+        while (totalEvents-- != 0) {
+            String event = scan.nextLine();
+            events.add(event);
+        }
+        
+        List<Student> students = priorities.getStudents(events);
+        
+        if (students.isEmpty()) {
+            System.out.println("EMPTY");
+        } else {
+            for (Student st: students) {
+                System.out.println(st.getName());
+            }
+        }
+    }
 }
