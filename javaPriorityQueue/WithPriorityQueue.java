12359bead537e4801d58c815df5b16b0155bd637
mridulraturi7 <mridulraturi7@gmail.com>
2020-06-13 11:49:39 +0530

    [Create Student Class] WithPriorityQueue.java

--- javaPriorityQueue/WithPriorityQueue.java
+++ javaPriorityQueue/WithPriorityQueue.java
@@ -4,6 +4,35 @@ import java.util.Scanner;
 import java.util.List;
 import java.util.ArrayList;
 
+class Student
+{
+    private int id;
+    private String name;
+    private double cgpa;
+
+    public Student(int id, String name, double cgpa)
+    {
+        this.id = id;
+        this.name = name;
+        this.cgpa = cgpa;
+    }
+
+    public int getId()
+    {
+        return id;
+    } 
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public double getCGPA()
+    {
+        return cgpa;
+    }
+}
+
 public class WithPriorityQueue {
 
     private final static Scanner scan = new Scanner(System.in);
