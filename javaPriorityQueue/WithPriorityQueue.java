5e0a89e85d27c804b0ddceb947c13239a11987bb
mridulraturi7 <mridulraturi7@gmail.com>
2020-06-13 11:47:37 +0530

    [Create main method] WithPriorityQueue.java

--- javaPriorityQueue/WithPriorityQueue.java
+++ javaPriorityQueue/WithPriorityQueue.java
@@ -2,6 +2,27 @@ package javaPriorityQueue;
 
 public class WithPriorityQueue {
 
+    private final static Scanner scan = new Scanner(System.in);
+    private final static Priorities priorities = new Priorities();
     
+    public static void main(String[] args) {
+        int totalEvents = Integer.parseInt(scan.nextLine());    
+        List<String> events = new ArrayList<>();
+        
+        while (totalEvents-- != 0) {
+            String event = scan.nextLine();
+            events.add(event);
+        }
+        
+        List<Student> students = priorities.getStudents(events);
+        
+        if (students.isEmpty()) {
+            System.out.println("EMPTY");
+        } else {
+            for (Student st: students) {
+                System.out.println(st.getName());
+            }
+        }
+    }
     
 }
