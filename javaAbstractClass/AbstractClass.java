3ee1a8cb962edbe5aa35cc1c6bf12ceb618ecfd1
mridulraturi7 <mridulraturi7@gmail.com>
2020-06-15 12:59:06 +0530

    [Checking] AbstractClass.java

--- javaAbstractClass/AbstractClass.java
+++ javaAbstractClass/AbstractClass.java
@@ -27,10 +27,11 @@ public class AbstractClass
 		//Book new_novel=new Book(); This line prHMain.java:25: error: Book is abstract; cannot be instantiated
 		Scanner sc=new Scanner(System.in);
 		String title=sc.nextLine();
-		MyBook new_novel=new MyBook();
+		Book new_novel=new MyBook();
 		new_novel.setTitle(title);
 		System.out.println("The title is: "+new_novel.getTitle());
-      	sc.close();
+          
+        sc.close();
 		
 	}   
 }
