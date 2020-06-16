4bc912660e39ff19ad0d96f0d39c829240268e29
mridulraturi7 <mridulraturi7@gmail.com>
2020-06-16 23:42:04 +0530

    [Add count method] InstanceOFExample.java

--- javaInstanceOfKeyword/InstanceOfExample.java
+++ javaInstanceOfKeyword/InstanceOfExample.java
@@ -19,6 +19,21 @@ class Hacker
 
 public class InstanceOfExample
 {
+
+    static String count(ArrayList mylist){
+        int a = 0,b = 0,c = 0;
+        for(int i = 0; i < mylist.size(); i++){
+           Object element=mylist.get(i);
+           if(element instanceof Student)
+              a++;
+           if(element instanceof Rockstar)
+              b++;
+           if(element instanceof Hacker)
+              c++;
+        }
+        String ret = Integer.toString(a)+" "+ Integer.toString(b)+" "+ Integer.toString(c);
+        return ret;
+     }
     public static void main(String []args){
         ArrayList mylist = new ArrayList();
         Scanner sc = new Scanner(System.in);
