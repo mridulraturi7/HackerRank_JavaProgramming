6670c997760152ec014fec1f974482dcb996e0f5
mridulraturi7 <mridulraturi7@gmail.com>
2020-06-16 23:37:37 +0530

    [Create main method] InstanceOfExample.java

--- javaInstanceOfKeyword/InstanceOfExample.java
+++ javaInstanceOfKeyword/InstanceOfExample.java
@@ -2,7 +2,16 @@ package javaInstanceOfKeyword;
 
 public class InstanceOfExample
 {
-
-    
-
+    public static void main(String []args){
+        ArrayList mylist = new ArrayList();
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for(int i=0; i<t; i++){
+           String s=sc.next();
+           if(s.equals("Student"))mylist.add(new Student());
+           if(s.equals("Rockstar"))mylist.add(new Rockstar());
+           if(s.equals("Hacker"))mylist.add(new Hacker());
+        }
+        System.out.println(count(mylist));
+     }
 }
