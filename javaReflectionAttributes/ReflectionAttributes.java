c6018ec04c2990f3709360f28e35ac0565a0b4de
mridulraturi7 <mridulraturi7@gmail.com>
2020-06-18 23:55:54 +0530

    [Create main method] ReflectionAttributes.java

--- javaReflectionAttributes/ReflectionAttributes.java
+++ javaReflectionAttributes/ReflectionAttributes.java
@@ -2,6 +2,21 @@ package javaReflectionAttributes;
 
 public class ReflectionAttributes {
 
-    
+    public static void main(String[] args){
+        Student std = new Student();
+        Class student = std.getClass(); //~~~Complete this line~~~;
+        Method[] methods = student.getDeclaredMethods();//~~~Complete this line~~~;
+
+        ArrayList<String> methodList = new ArrayList<>();
+        //for(~~~Complete this line~~~){
+        for(Method method : methods){
+            //methodList.add(~~~Complete this line~~~);
+            methodList.add(method.getName());
+        }
+        Collections.sort(methodList);
+        for(String name: methodList){
+            System.out.println(name);
+        }
+    }
     
 }
