d6ff412fc5f58be13133ef73afc4930670d90206
mridulraturi7 <mridulraturi7@gmail.com>
2020-06-21 21:23:36 +0530

    [Logic Implemented] PrimeChecker.java

--- javaPrimeChecker/PrimeChecker.java
+++ javaPrimeChecker/PrimeChecker.java
@@ -16,6 +16,35 @@ class Prime
         }
         System.out.println();
     }
+
+    boolean isPrime(int n)
+    {
+        if(n <= 1)
+        {
+            return false;
+        }
+
+        else if(n == 2)
+        {
+            return true;
+        }
+
+        else if(n%2 == 0)
+        {
+            return false;
+        }
+
+        int sqroot = (int)Math.sqrt(n);
+
+        for(int i = 2; i <= sqroot; i++)
+        {
+            if(n%i == 0)
+            {
+                return false;
+            }           
+        }
+        return true;
+    }
 }
 
 public class PrimeChecker {
