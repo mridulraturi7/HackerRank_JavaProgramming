4b331b2c47a1707c4c24f1b44fa751733a038978
mridulraturi7 <mridulraturi7@gmail.com>
2020-06-23 23:16:31 +0530

    [Create main method] FactoryPattern.java

--- javaFactoryPattern/FactoryPattern.java
+++ javaFactoryPattern/FactoryPattern.java
@@ -2,6 +2,25 @@ package javaFactoryPattern;
 
 public class FactoryPattern {
 
-    
+    public static void main(String args[]){
+        Do_Not_Terminate.forbidExit();
+
+    try{
+
+        Scanner sc=new Scanner(System.in);
+        //creating the factory
+        FoodFactory foodFactory = new FoodFactory();
+
+        //factory instantiates an object
+        Food food = foodFactory.getFood(sc.nextLine());
+
+        
+        System.out.println("The factory returned "+food.getClass());
+        System.out.println(food.getType());
+    }
+    catch (Do_Not_Terminate.ExitTrappedException e) {
+        System.out.println("Unsuccessful Termination!!");
+    }
+ }
     
 }
