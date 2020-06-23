6b2d0b3cc28c171ed68612d5b97cb017cbd27260
mridulraturi7 <mridulraturi7@gmail.com>
2020-06-23 23:26:41 +0530

    [Modify Indentation] FactoryPatter.java

--- javaFactoryPattern/FactoryPattern.java
+++ javaFactoryPattern/FactoryPattern.java
@@ -3,26 +3,31 @@ package javaFactoryPattern;
 import java.util.Scanner;
 import java.security.Permission;
 
-interface Food {
+interface Food 
+{
     public String getType();
 }
 
-class Pizza implements Food {
-    public String getType() {
+class Pizza implements Food 
+{
+    public String getType() 
+    {
     return "Someone ordered a Fast Food!";
     }
-   }
+}
 
-   class Cake implements Food {
-
-    public String getType() {
+class Cake implements Food 
+{
+    public String getType() 
+    {
     return "Someone ordered a Dessert!";
     }
-   }
+}
 
-   class FoodFactory {
-    public Food getFood(String order) {
-
+class FoodFactory 
+{
+    public Food getFood(String order) 
+    {
         if(order.equals("pizza"))
         {
             return new Pizza();
@@ -34,52 +39,54 @@ class Pizza implements Food {
         }
 
         return null;
-
-//Write your code here
-
-}//End of getFood method
-
+        //Write your code here
+    }//End of getFood method
 }//End of factory class
 
-public class FactoryPattern {
-
-    public static void main(String args[]){
+public class FactoryPattern 
+{
+    public static void main(String args[])
+    {
         Do_Not_Terminate.forbidExit();
 
-    try{
+        try
+        {   
+            Scanner sc=new Scanner(System.in);
+            //creating the factory
+            FoodFactory foodFactory = new FoodFactory();
 
-        Scanner sc=new Scanner(System.in);
-        //creating the factory
-        FoodFactory foodFactory = new FoodFactory();
-
-        //factory instantiates an object
-        Food food = foodFactory.getFood(sc.nextLine());
+            //factory instantiates an object
+            Food food = foodFactory.getFood(sc.nextLine());
 
         
-        System.out.println("The factory returned "+food.getClass());
-        System.out.println(food.getType());
+            System.out.println("The factory returned "+food.getClass());
+            System.out.println(food.getType());
 
-        sc.close();
+            sc.close();
+        }
+        catch (Do_Not_Terminate.ExitTrappedException e) 
+        {
+            System.out.println("Unsuccessful Termination!!");
+        }
     }
-    catch (Do_Not_Terminate.ExitTrappedException e) {
-        System.out.println("Unsuccessful Termination!!");
-    }
- }
-    
 }
 
-class Do_Not_Terminate {
-		 
-    public static class ExitTrappedException extends SecurityException {
-
+class Do_Not_Terminate 
+{		 
+    public static class ExitTrappedException extends SecurityException 
+    {
         private static final long serialVersionUID = 1L;
     }
  
-    public static void forbidExit() {
-        final SecurityManager securityManager = new SecurityManager() {
+    public static void forbidExit() 
+    {
+        final SecurityManager securityManager = new SecurityManager() 
+        {
             @Override
-            public void checkPermission(Permission permission) {
-                if (permission.getName().contains("exitVM")) {
+            public void checkPermission(Permission permission) 
+            {
+                if (permission.getName().contains("exitVM")) 
+                {
                     throw new ExitTrappedException();
                 }
             }
