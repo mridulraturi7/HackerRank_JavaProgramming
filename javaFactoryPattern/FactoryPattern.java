0e3e0ef7719c3ec3b43fb18cb2c1fe7260095aab
mridulraturi7 <mridulraturi7@gmail.com>
2020-06-23 23:19:45 +0530

    [Create Pizza Class] FactoryPattern.java

--- javaFactoryPattern/FactoryPattern.java
+++ javaFactoryPattern/FactoryPattern.java
@@ -6,6 +6,12 @@ interface Food {
     public String getType();
 }
 
+class Pizza implements Food {
+    public String getType() {
+    return "Someone ordered a Fast Food!";
+    }
+   }
+
 public class FactoryPattern {
 
     public static void main(String args[]){
