f7cb3285b80706fa0b07561e158f18dd199f07e5
mridulraturi7 <mridulraturi7@gmail.com>
2020-06-24 18:44:06 +0530

    [Create class Tree] VisitorPattern.java

--- javaVisitorPattern/VisitorPattern.java
+++ javaVisitorPattern/VisitorPattern.java
@@ -4,6 +4,33 @@ enum Color {
     RED, GREEN
 }
 
+abstract class Tree {
+
+    private int value;
+    private Color color;
+    private int depth;
+
+    public Tree(int value, Color color, int depth) {
+        this.value = value;
+        this.color = color;
+        this.depth = depth;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public int getDepth() {
+        return depth;
+    }
+
+    public abstract void accept(TreeVis visitor);
+}
+
 public class VisitorPattern {
 
     public static void main(String[] args) {
