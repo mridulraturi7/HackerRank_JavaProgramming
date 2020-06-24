2eb53634cf0c07c06d4d44d0def1ea4b08070319
mridulraturi7 <mridulraturi7@gmail.com>
2020-06-24 18:18:03 +0530

    [Create Singleton Class] SingletonPattern.java

--- javaSingletonPattern/SingletonPattern.java
+++ javaSingletonPattern/SingletonPattern.java
@@ -1,7 +1,25 @@
 package javaSingletonPattern;
 
+class Singleton{
+    
+    public String str;
+    
+    private static Singleton singleObj = new Singleton();
+    
+    private Singleton()
+    {
+        
+    }
+    
+    public static Singleton getSingleInstance()
+    {
+        return singleObj;
+    }
+
+}
+
 public class SingletonPattern {
 
-    
+
     
 }
