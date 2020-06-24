4d1f41070e4547a2f00844ab7a1bbaade68126e2
mridulraturi7 <mridulraturi7@gmail.com>
2020-06-24 18:42:12 +0530

    [Crrate main method] VisitoPattern.java

--- javaVisitorPattern/VisitorPattern.java
+++ javaVisitorPattern/VisitorPattern.java
@@ -2,6 +2,23 @@ package javaVisitorPattern;
 
 public class VisitorPattern {
 
-    
+    public static void main(String[] args) {
+        Tree root = solve();
+      SumInLeavesVisitor vis1 = new SumInLeavesVisitor();
+        ProductOfRedNodesVisitor vis2 = new ProductOfRedNodesVisitor();
+        FancyVisitor vis3 = new FancyVisitor();
+
+        root.accept(vis1);
+        root.accept(vis2);
+        root.accept(vis3);
+
+        int res1 = vis1.getResult();
+        int res2 = vis2.getResult();
+        int res3 = vis3.getResult();
+
+        System.out.println(res1);
+       System.out.println(res2);
+      System.out.println(res3);
+  }
     
 }
