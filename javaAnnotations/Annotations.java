4daf13a6d5cee1729211f89b0ffcf6dfc1ad6df0
mridulraturi7 <mridulraturi7@gmail.com>
2020-06-25 17:40:59 +0530

    [Create main method] Annotations.java

--- javaAnnotations/Annotations.java
+++ javaAnnotations/Annotations.java
@@ -2,6 +2,36 @@ package javaAnnotations;
 
 public class Annotations {
 
-    
+    public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int testCases = Integer.parseInt(in.nextLine());
+		while (testCases > 0) {
+			String role = in.next();
+			int spend = in.nextInt();
+			try {
+				Class annotatedClass = FamilyMember.class;
+				Method[] methods = annotatedClass.getMethods();
+				for (Method method : methods) {
+					if (method.isAnnotationPresent(FamilyBudget.class)) {
+						FamilyBudget family = method
+								.getAnnotation(FamilyBudget.class);
+						String userRole = family.userRole();
+						int budgetLimit = //~~Complete this line~~;
+						if (userRole.equals(role)) {
+                            if(~~Complete this line~~){
+								method.invoke(FamilyMember.class.newInstance(),
+										budgetLimit, spend);
+							}else{
+								System.out.println("Budget Limit Over");
+							}
+						}
+					}
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			testCases--;
+		}
+	}
     
 }
