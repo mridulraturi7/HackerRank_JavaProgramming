87028e892adb38d04ef38b542d82ad7b6561c045
mridulraturi7 <mridulraturi7@gmail.com>
2020-06-25 23:21:12 +0530

    [Create main method] CovariantReturnType.java

--- javaCovariantReturnTypes/CovariantreturnType.java
+++ javaCovariantReturnTypes/CovariantreturnType.java
@@ -2,6 +2,20 @@ package javaCovariantReturnTypes;
 
 public class CovariantReturnType {
 
-    
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String s = reader.readLine().trim();
+        Region region = null;
+        switch (s) {
+          case "WestBengal":
+            region = new WestBengal();
+            break;
+          case "AndhraPradesh":
+            region = new AndhraPradesh();
+            break;
+        }
+        Flower flower = region.yourNationalFlower();
+        System.out.println(flower.whatsYourName());
+      }
     
 }
