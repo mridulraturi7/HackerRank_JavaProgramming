6304224ab6793cc78dff576c2aa8c305f993dba3
mridulraturi7 <mridulraturi7@gmail.com>
2020-06-25 17:44:38 +0530

    [Create Class FamilyMember] Annotations.java

--- javaAnnotations/Annotations.java
+++ javaAnnotations/Annotations.java
@@ -10,6 +10,22 @@ import java.util.Scanner;
     int budget() default 0;
 }
 
+class FamilyMember {
+    @FamilyBudget(userRole = "SENIOR", budget = 100)
+	public void seniorMember(int budget, int moneySpend) {
+		System.out.println("Senior Member");
+		System.out.println("Spend: " + moneySpend);
+		System.out.println("Budget Left: " + (budget - moneySpend));
+	}
+
+	@FamilyBudget(userRole = "JUNIOR", budget = 50)
+    public void juniorUser(int budget, int moneySpend) {
+		System.out.println("Junior Member");
+		System.out.println("Spend: " + moneySpend);
+		System.out.println("Budget Left: " + (budget - moneySpend));
+	}
+}
+
 public class Annotations {
 
     public static void main(String[] args) {
