66ccc2a6d94a09f6f2958a22882023753ec36733
mridulraturi7 <mridulraturi7@gmail.com>
2020-06-26 12:08:49 +0530

    [Resolve Errors] Lambda.java

--- javaLambdaExpressions/Lambda.java
+++ javaLambdaExpressions/Lambda.java
@@ -8,10 +8,12 @@ interface PerformOperation {
     boolean check(int a);
    }
 
-   class MyMath {
+class MyMath 
+{
     public static boolean checker(PerformOperation p, int num) {
      return p.check(num);
     }
+}
 
 public class Lambda {
 
