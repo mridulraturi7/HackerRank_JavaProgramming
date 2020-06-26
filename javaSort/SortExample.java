a668bd3446de563695b1706c19e90eb2c75e0984
mridulraturi7 <mridulraturi7@gmail.com>
2020-06-26 11:56:44 +0530

    [Modify Indentation] SortExample.java

--- javaSort/SortExample.java
+++ javaSort/SortExample.java
@@ -7,40 +7,42 @@ class Student
 	private int id;
 	private String fname;
 	private double cgpa;
-    public Student(int id, String fname, double cgpa) 
-    {
+  
+  public Student(int id, String fname, double cgpa) 
+  {
 		super();
 		this.id = id;
 		this.fname = fname;
 		this.cgpa = cgpa;
-    }
+  }
     
-    public int getId() 
-    {
+  public int getId() 
+  {
 		return id;
-    }
+  }
     
-    public String getFname() 
-    {
-		return fname;
-    }
+  public String getFname() 
+  {
+	  return fname;
+  }
     
-    public double getCgpa() 
-    {
+  public double getCgpa() 
+  {
 		return cgpa;
 	}
 }
 
-public class SortExample {
-
+public class SortExample 
+{
     public static void main(String[] args)
     {
 		Scanner in = new Scanner(System.in);
 		int testCases = Integer.parseInt(in.nextLine());
 		
 		List<Student> studentList = new ArrayList<Student>();
-        while(testCases>0)
-        {
+    
+    while(testCases>0)
+    {
 			int id = in.nextInt();
 			String fname = in.next();
 			double cgpa = in.nextDouble();
@@ -49,48 +51,47 @@ public class SortExample {
 			studentList.add(st);
 			
 			testCases--;
-        }
+    }
         
-        in.close();
+    in.close();
 
-        Comparator<Student> myComparator = new Comparator<Student>()
+    Comparator<Student> myComparator = new Comparator<Student>()
+    {
+      public int compare(Student a, Student b)
+      {
+        double g1 = a.getCgpa();
+        double g2 = b.getCgpa();
+
+        int gCompare = Double.compare(g2, g1);
+
+        if(gCompare == 0)
         {
-            public int compare(Student a, Student b)
-            {
-                double g1 = a.getCgpa();
-                double g2 = b.getCgpa();
-
-                int gCompare = Double.compare(g2, g1);
-
-                if(gCompare == 0)
-                {
-                    int nCompare = a.getFname().compareTo(b.getFname());
+          int nCompare = a.getFname().compareTo(b.getFname());
                     
-                    if(nCompare == 0)
-                    {
-                        return Integer.compare(a.getId(), b.getId());
-                    }
+          if(nCompare == 0)
+          {
+            return Integer.compare(a.getId(), b.getId());
+          }
 
-                    else
-                    {
-                        return nCompare;
-                    }
+          else
+          {
+            return nCompare;
+          }
 
-                }
+        }
 
-                else
-                {
-                    return gCompare;
-                }
-            }
-        };
-
-        Collections.sort(studentList, myComparator);
-      
-        for(Student st: studentList)
+        else
         {
+          return gCompare;
+        }
+      }
+    };
+
+    Collections.sort(studentList, myComparator);
+      
+    for(Student st: studentList)
+    {
 			System.out.println(st.getFname());
 		}
 	}
-    
 }
