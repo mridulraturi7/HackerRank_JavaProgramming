05cee8e244f40b5f7954bb731eac5402877db5cd
mridulraturi7 <mridulraturi7@gmail.com>
2020-06-26 18:19:16 +0530

    [Remove comment] MD5Example.java

--- javaMD5/MD5Example.java
+++ javaMD5/MD5Example.java
@@ -8,7 +8,6 @@ import java.security.NoSuchAlgorithmException;
 public class MD5Example {
 
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
         Scanner scan = new Scanner(System.in);
 
         String str = scan.next();
