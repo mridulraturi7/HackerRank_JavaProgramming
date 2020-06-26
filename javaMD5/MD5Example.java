5a69a09140ad1b391cb92880dbccbf0e4c76eb94
mridulraturi7 <mridulraturi7@gmail.com>
2020-06-26 18:09:33 +0530

    [Create main method] MD5Example.java

--- javaMD5/MD5Example.java
+++ javaMD5/MD5Example.java
@@ -2,6 +2,15 @@ package javaMD5;
 
 public class MD5Example {
 
-    
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scan = new Scanner(System.in);
+
+        String str = scan.next();
+
+        System.out.println(getMD5(str));
+
+        scan.close();
+    }
     
 }
