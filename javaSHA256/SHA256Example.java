bee8c67fa8f993874f795c4facd4e5ecbcae961c
mridulraturi7 <mridulraturi7@gmail.com>
2020-06-27 11:39:31 +0530

    [Rename] SHA256.java -> SHA256Example.java

--- javaSHA256/SHA256Example.java
+++ javaSHA256/SHA256Example.java
@@ -2,7 +2,7 @@ package javaSHA256;
 
 import java.util.Scanner;
 
-public class SHA256 {
+public class SHA256Example {
 
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
