27ee5b0abcee598f33c99b240c3493b50853b97b
mridulraturi7 <mridulraturi7@gmail.com>
2020-06-27 11:37:03 +0530

    [Create main method] SHA256.java

--- javaSHA256/SHA256.java
+++ javaSHA256/SHA256.java
@@ -2,6 +2,12 @@ package javaSHA256;
 
 public class SHA256 {
 
-    
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+
+        Scanner scan = new Scanner(System.in);
+
+        String str = scan.next();
+    }
     
 }
