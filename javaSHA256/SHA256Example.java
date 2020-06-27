2c16d02bcd50410f70b65fe1f739672b7f6ba59b
mridulraturi7 <mridulraturi7@gmail.com>
2020-06-27 11:43:17 +0530

    [Remove Comment] SHA256.java

--- javaSHA256/SHA256Example.java
+++ javaSHA256/SHA256Example.java
@@ -9,8 +9,6 @@ public class SHA256Example
 {
     public static void main(String[] args) 
     {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
-
         Scanner scan = new Scanner(System.in);
 
         String str = scan.next();
