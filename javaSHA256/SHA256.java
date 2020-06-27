8ad6b4b9e7f226744a24bcb0a6432790a823eae2
mridulraturi7 <mridulraturi7@gmail.com>
2020-06-27 11:37:52 +0530

    [Logic Implemented] SHA256.java

--- javaSHA256/SHA256.java
+++ javaSHA256/SHA256.java
@@ -8,6 +8,33 @@ public class SHA256 {
         Scanner scan = new Scanner(System.in);
 
         String str = scan.next();
+
+        findSHA256Hash(str);
+    }
+
+    public static void findSHA256Hash(String str)
+    {
+        try
+        {
+            MessageDigest mD = MessageDigest.getInstance("SHA-256");
+
+            byte[] messageDigest = mD.digest(str.getBytes());
+
+            BigInteger bD = new BigInteger(1, messageDigest);
+
+            String hashValue = bD.toString(16);
+
+            while(hashValue.length() < 32)
+            {
+                hashValue = "0" + hashValue;
+            }
+
+            System.out.println(hashValue);
+        }
+        catch(NoSuchAlgorithmException aE)
+        {
+            throw new RuntimeException(aE);
+        }
     }
     
 }
