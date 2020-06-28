1a556831045029b4c13aeb4f521ce2d1548b62b9
mridulraturi7 <mridulraturi7@gmail.com>
2020-06-28 13:12:54 +0530

    [Logic Complete] Solution2.java

--- javaBasicCertification/theAdderClass/Solution2.java
+++ javaBasicCertification/theAdderClass/Solution2.java
@@ -21,6 +21,9 @@ public class Solution2 {
             a = scan.nextInt();
             b = scan.nextInt();
         }
+
+        Calculator adderObject = new Adder();
+        System.out.println("The sum is: " + adderObject.add(a, b));
     }
     
 }
