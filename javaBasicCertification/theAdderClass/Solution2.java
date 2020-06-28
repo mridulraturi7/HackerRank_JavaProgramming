2b6a87236b03d798a17ef9c6b9248dabb3be11ee
mridulraturi7 <mridulraturi7@gmail.com>
2020-06-28 13:13:36 +0530

    [Modify Indentation] Solution2.java

--- javaBasicCertification/theAdderClass/Solution2.java
+++ javaBasicCertification/theAdderClass/Solution2.java
@@ -2,28 +2,31 @@ package javaBasicCertification.theAdderClass;
 
 import java.util.Scanner;
 
-abstract class Calculator{
+abstract class Calculator
+{
     abstract int add(int a, int b);
 }
 
-class Adder extends Calculator{
+class Adder extends Calculator
+{
     public int add(int a, int b)
     {
         return a+b;
     }
 }
 
-public class Solution2 {
-
-    public static void main(String[] args) {
+public class Solution2 
+{
+    public static void main(String[] args) 
+    {
         int a, b;
-        try (Scanner scan = new Scanner(System.in)){
+        try (Scanner scan = new Scanner(System.in))
+        {
             a = scan.nextInt();
             b = scan.nextInt();
         }
 
         Calculator adderObject = new Adder();
         System.out.println("The sum is: " + adderObject.add(a, b));
-    }
-    
+    }  
 }
