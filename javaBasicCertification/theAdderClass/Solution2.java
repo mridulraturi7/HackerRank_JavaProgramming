1f0cda0ef5d98d9032c1c184e90d0c21f05a153e
mridulraturi7 <mridulraturi7@gmail.com>
2020-06-28 13:09:08 +0530

    [Create main method] Solution2.java

--- javaBasicCertification/theAdderClass/Solution2.java
+++ javaBasicCertification/theAdderClass/Solution2.java
@@ -1,7 +1,15 @@
 package javaBasicCertification.theAdderClass;
 
+import java.util.Scanner;
+
 public class Solution2 {
 
-    
+    public static void main(String[] args) {
+        int a, b;
+        try (Scanner scan = new Scanner(System.in)){
+            a = scan.nextInt();
+            b = scan.nextInt();
+        }
+    }
     
 }
