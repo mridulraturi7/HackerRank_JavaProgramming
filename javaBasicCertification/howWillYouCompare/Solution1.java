93407d1c0c88662fa50d18f2122307dba1645cfe
mridulraturi7 <mridulraturi7@gmail.com>
2020-06-28 12:59:43 +0530

    [Create main method] Solution1.java

--- javaBasicCertification/howWillYouCompare/Solution1.java
+++ javaBasicCertification/howWillYouCompare/Solution1.java
@@ -2,6 +2,60 @@ package javaBasicCertification.howWillYouCompare;
 
 public class Solution1 {
 
-    
-    
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+
+        Comparator comp = new Comparator();
+
+        int testCases = Integer.parseInt(scan.nextLine());
+
+        while(testCases-- > 0)
+        {
+            int condition = Integer.parseInt(scan.nextLine());
+            switch(condition){
+                case 1:
+                    String s1 = scan.nextLine().trim();
+                    String s2 = scan.nextLine().trim();
+
+                    System.out.println( (comp.compare(s1, s2)) ? "Same" : "Different" );
+                    break;
+            
+                case 2:
+                    int num1 = scan.nextInt();
+                    int num2 = scan.nextInt();
+
+                    System.out.println( (comp.compare(num1, num2)) ? "Same" : "Different" );
+                
+                    if(scan.hasNext()){
+                        scan.nextLine();
+                    }
+                    break;
+
+                case 3:
+                    int[] array1 = new int[scan.nextInt()];
+                    int[] array2 = new int[scan.nextInt()];
+
+                    for(int i = 0; i < array1.length; i++)
+                    {
+                        array1[i] = scan.nextInt();
+                    }
+
+                    for(int i = 0; i < array2.length; i++)
+                    {
+                        array2[i] = scan.nextInt();
+                    }
+
+                    System.out.println( (comp.compare(array1, array2)) ? "Same" : "Different" );
+
+                    if(scan.hasNext()){
+                        scan.nextLine();
+                    }
+                    break;
+                
+                default:
+                    System.out.println("Invalid input.");
+            }
+        }
+    }
 }
