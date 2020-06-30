bcbb963d2d0f177a76d036180cac9ae9747972c4
mridulraturi7 <mridulraturi7@gmail.com>
2020-06-30 11:14:30 +0530

    [Update] MapExample.java

--- javaMap/MapExample.java
+++ javaMap/MapExample.java
@@ -4,14 +4,15 @@ import java.util.Scanner;
 import java.util.Map;
 import java.util.HashMap;
 
-public class MapExample {
-
+public class MapExample 
+{
     public static void main(String []argh)
     {
-        Map<String, Integer> myMap = new HashMap<>();
+		Map<String, Integer> myMap = new HashMap<>();
 		Scanner in = new Scanner(System.in);
 		int n=in.nextInt();
 		in.nextLine();
+		
 		for(int i = 0; i < n; i++)
 		{
 			String name = in.nextLine();
@@ -19,18 +20,19 @@ public class MapExample {
 			myMap.put(name, phone);
 			in.nextLine();
 		}
+		
 		while(in.hasNext())
 		{
 			String s=in.nextLine();
-            if(myMap.containsKey(s))
+			if(myMap.containsKey(s))
+			{
+				System.out.println(s + "=" + myMap.get(s));
+			}
+			else
             {
-                System.out.println(s + "=" + myMap.get(s));
-            }
-            else
-            {
-            	System.out.println("Not found");
-        	}
-    	}
-    	in.close();
-    }
+				System.out.println("Not found");
+			}
+		}
+		in.close();
+	}
 }
