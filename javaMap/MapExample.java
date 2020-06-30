e0898db97aafc7618a9bcfef514ba5f88a86cd0f
mridulraturi7 <mridulraturi7@gmail.com>
2020-06-30 11:07:42 +0530

    [Modify] MapExample.java

--- javaMap/MapExample.java
+++ javaMap/MapExample.java
@@ -9,28 +9,28 @@ public class MapExample {
     public static void main(String []argh)
     {
         Map<String, Integer> myMap = new HashMap<>();
-	Scanner in = new Scanner(System.in);
-	int n=in.nextInt();
-	in.nextLine();
-	for(int i = 0; i < n; i++)
-	{
-		String name = in.nextLine();
-		int phone = in.nextInt();
-		myMap.put(name, phone);
+		Scanner in = new Scanner(System.in);
+		int n=in.nextInt();
 		in.nextLine();
-	}
-	while(in.hasNext())
-	{
-		String s=in.nextLine();
-            	if(myMap.containsKey(s))
-            	{
-                	System.out.println(s + "=" + myMap.get(s));
-            	}
-            	else
-            	{
-                	System.out.println("Not found");
-            	}
-        }
-        in.close();
-     }
+		for(int i = 0; i < n; i++)
+		{
+			String name = in.nextLine();
+			int phone = in.nextInt();
+			myMap.put(name, phone);
+			in.nextLine();
+		}
+		while(in.hasNext())
+		{
+			String s=in.nextLine();
+            if(myMap.containsKey(s))
+            {
+                System.out.println(s + "=" + myMap.get(s));
+            }
+            else
+            {
+            	System.out.println("Not found");
+        	}
+    	}
+    	in.close();
+    }
 }
