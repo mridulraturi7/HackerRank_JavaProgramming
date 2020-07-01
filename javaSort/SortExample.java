1db1b97a4dbac1289047235cc43cff55954c1e67
mridulraturi7 <mridulraturi7@gmail.com>
2020-07-01 11:16:02 +0530

    [Modify Indentation] SortExample.java

--- javaSort/SortExample.java
+++ javaSort/SortExample.java
@@ -4,21 +4,21 @@ import java.util.*;
 
 class Student
 {
-	private int id;
-	private String fname;
-	private double cgpa;
+  private int id;
+  private String fname;
+  private double cgpa;
   
   public Student(int id, String fname, double cgpa) 
   {
-		super();
-		this.id = id;
-		this.fname = fname;
-		this.cgpa = cgpa;
+	  super();
+	  this.id = id;
+	  this.fname = fname;
+	  this.cgpa = cgpa;
   }
     
   public int getId() 
   {
-		return id;
+	  return id;
   }
     
   public String getFname() 
@@ -32,20 +32,20 @@ class Student
 	}
 }
 
-public class SortExample 
+public class SortExample
 {
-    public static void main(String[] args)
-    {
-		Scanner in = new Scanner(System.in);
+  public static void main(String[] args)
+  {
+    Scanner in = new Scanner(System.in);
 		int testCases = Integer.parseInt(in.nextLine());
 		
-		List<Student> studentList = new ArrayList<Student>();
+	  List<Student> studentList = new ArrayList<Student>();
     
     while(testCases>0)
     {
-			int id = in.nextInt();
-			String fname = in.next();
-			double cgpa = in.nextDouble();
+		  int id = in.nextInt();
+		  String fname = in.next();
+		  double cgpa = in.nextDouble();
 			
 			Student st = new Student(id, fname, cgpa);
 			studentList.add(st);
@@ -91,7 +91,7 @@ public class SortExample
       
     for(Student st: studentList)
     {
-			System.out.println(st.getFname());
+		  System.out.println(st.getFname());
 		}
 	}
 }
